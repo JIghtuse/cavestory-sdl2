f7186e78195c87a962e6b90d55a3edf285f4ca4a
Boris Egorov <jightuse@gmail.com>
2014-01-21 19:38:09 +0700

    Make Sprite class virtual for inheriting from it

--- src/sprite.h
+++ src/sprite.h
@@ -1,6 +1,7 @@
 #ifndef SPRITE_H
 #define SPRITE_H
 
+#include <chrono>
 #include <string>
 #include <SDL2/SDL.h>
 
@@ -10,16 +11,19 @@ struct Sprite {
     Sprite(const std::string& file_path,
             int source_x, int source_y,
             int width, int height, SDL_Renderer* ren);
-    ~Sprite();
+    virtual ~Sprite();
 
     Sprite(const Sprite&)=delete;
     Sprite operator=(const Sprite&)=delete;
 
+    virtual void update(std::chrono::duration<double>) {};
     void draw(Graphics& graphics, int x, int y);
 
 private:
     SDL_Renderer *renderer_;
     SDL_Texture *texture_;
+
+protected:
     SDL_Rect source_rect_;
 };
 
