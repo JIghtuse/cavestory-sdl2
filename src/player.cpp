00b11cfa71da3f97320fb87ff22fef2ea4424116
Boris Egorov <jightuse@gmail.com>
2014-01-27 04:01:34 +0700

    Implemented jumping and fake collision with floor

--- src/player.cpp
+++ src/player.cpp
@@ -8,6 +8,8 @@
 const double kSlowdownFactor = 0.8;
 const double kWalkingAcceleration = 0.0012; // (pixels/ms) / ms
 const double kMaxSpeedX = 0.325;            // pixels / ms
+const double kMaxSpeedY = 0.325;            // pixels / ms
+const double kGravity = 0.0012;             // (pixels / ms) / ms
 const double kJumpSpeed = 0.325;            // pixels / ms
 const auto kJumpTime = std::chrono::milliseconds(275);
 
@@ -22,6 +24,8 @@ Player::Player(Graphics& graphics, Vector<int> pos) :
     velocity_{0.0, 0.0},
     acceleration_{0.0, 0.0},
     horizontal_facing_{HorizontalFacing::LEFT},
+    is_on_ground_{false},
+    jump_(),
     sprites_()
 {
     initializeSprites(graphics);
@@ -35,15 +39,29 @@ Player::~Player()
 void Player::update(std::chrono::milliseconds elapsed_time)
 {
     sprites_[getSpriteState()]->update(elapsed_time);
+    jump_.update(elapsed_time);
+
     pos_.x += round(velocity_.x * elapsed_time.count());
     velocity_.x += acceleration_.x * elapsed_time.count();
     if (acceleration_.x < 0.0) {
         velocity_.x = std::max(velocity_.x, -kMaxSpeedX);
     } else if (acceleration_.x > 0.0) {
         velocity_.x = std::min(velocity_.x, kMaxSpeedX);
-    } else {
+    } else if (is_on_ground_) {
         velocity_.x *= kSlowdownFactor;
     }
+
+    pos_.y += round(velocity_.y * elapsed_time.count());
+    if (!jump_.is_active()) {
+        velocity_.y += std::min(kGravity * elapsed_time.count(), kMaxSpeedY);
+    }
+
+    // FIXME: implement correct collision instead of code below
+    if (pos_.y >= 320) {
+        pos_.y = 320;
+        velocity_.y = 0.0;
+    }
+    is_on_ground_ = pos_.y == 320;
 }
 
 void Player::draw(Graphics& graphics)
@@ -70,16 +88,17 @@ void Player::stopMoving()
 
 void Player::startJump()
 {
-    //if (is_on_ground()) {
-        //jump_.reset();
-        //velocity_.y = -kJumpSpeed;
-    //} else if (velocity_.y < 0.0) {
-        //jump_.reactivate();
-    //}
+    if (is_on_ground()) {
+        jump_.reset();
+        velocity_.y = -kJumpSpeed;
+    } else if (velocity_.y < 0.0) {
+        jump_.reactivate();
+    }
 }
 
 void Player::stopJump()
 {
+    jump_.deactivate();
 }
 
 void Player::initializeSprites(Graphics& graphics)
@@ -114,4 +133,20 @@ Player::SpriteState Player::getSpriteState()
             acceleration_.x == 0.0 ? MotionType::STANDING
                                    : MotionType::WALKING,
             horizontal_facing_);
+}
+
+void Player::Jump::reset()
+{
+    time_remaining_ = kJumpTime;
+    reactivate();
+}
+
+void Player::Jump::update(std::chrono::milliseconds elapsed_time)
+{
+    if (Player::Jump::active_) {
+        time_remaining_ -= elapsed_time;
+        if (time_remaining_.count() <= 0) {
+            Player::Jump::active_ = false;
+        }
+    }
 }
--- src/player.h
+++ src/player.h
@@ -43,13 +43,30 @@ private:
    };
    friend bool operator<(const SpriteState& a, const SpriteState& b);
 
+   struct Jump {
+       Jump() : time_remaining_(0), active_(false) {}
+       void update(std::chrono::milliseconds elapsed_time);
+       void reset();
+       void reactivate() { active_ = time_remaining_.count() > 0; }
+       void deactivate() { active_ = false; }
+       bool is_active() const { return active_; }
+   private:
+       std::chrono::duration<double,std::milli> time_remaining_;
+       bool active_;
+   };
+
    void initializeSprites(Graphics& graphics);
    SpriteState getSpriteState();
+
+   bool is_on_ground() const { return is_on_ground_; }
 
    Vector<int> pos_;
    Vector<double> velocity_;
    Vector<double> acceleration_;
    HorizontalFacing horizontal_facing_;
+   bool is_on_ground_;
+   Jump jump_;
+
    std::map<SpriteState, std::unique_ptr<Sprite> > sprites_;
    static int kTileSize;
 };
