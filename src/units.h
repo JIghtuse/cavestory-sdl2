f91424380022e77cb7a462cf93d55974352fd8fc
Boris Egorov <jightuse@gmail.com>
2014-02-08 15:45:32 +0700

    Add spacial units definitions: Game, Pixel, Tile

---
+++ src/units.h
@@ -0,0 +1,30 @@
+#ifndef UNITS_H_
+#define UNITS_H_
+
+#include <cmath>
+
+namespace units {
+    typedef double Game; // intrinsic units of position
+    typedef int Pixel;
+    typedef unsigned int Tile;
+
+    namespace {
+        const Game kTileSize{32.0};
+    }
+
+    inline Pixel gameToPixel(Game game) {
+        // TODO: quit assuming 16x16
+        return Pixel(round(game / 2));
+    }
+    inline Tile gameToTile(Game game) {
+        return Tile(game / kTileSize);
+    }
+    inline Game tileToGame(Tile tile) {
+        return tile * kTileSize;
+    }
+    inline Pixel tileToPixel(Tile tile) {
+        return gameToPixel(tileToGame(tile));
+    }
+} // units
+
+#endif /* UNITS_H_ */
