73504e87a7329af23126011ba8e1a551e028c8a2
Boris Egorov <jightuse@gmail.com>
2014-02-15 15:52:40 +0700

    Fix Backdrop inheritance: use virtual dtor

--- src/backdrop.h
+++ src/backdrop.h
@@ -5,12 +5,16 @@
 #include "graphics.h"
 
 struct Backdrop {
+    virtual ~Backdrop() {};
     virtual void draw(Graphics& graphics) const = 0;
 };
 
 struct FixedBackdrop : public Backdrop {
     FixedBackdrop(const std::string& path, Graphics& graphics);
+    ~FixedBackdrop() {};
     void draw(Graphics& graphics) const;
+    FixedBackdrop(const FixedBackdrop&)=delete;
+    FixedBackdrop& operator=(const FixedBackdrop&)=delete;
     // TODO: does texture_id_ leaks when FixedBackdrop destroyed?
 private:
     SDL_Texture* texture_id_;
