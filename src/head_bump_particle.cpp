db1bb4e42516e798aaa8025be3685ed6419bd36d
Boris Egorov <jightuse@gmail.com>
2014-05-23 14:17:59 +0700

    Draw a second particle in HeadBumpParticle
    To move a particle around a circle we add an operator+ to Vector class
    which sum up a two Vectors.

--- src/head_bump_particle.cpp
+++ src/head_bump_particle.cpp
@@ -8,6 +8,7 @@ const units::Game kSourceHeight{6};
 const std::chrono::milliseconds kLifeTime{600};
 const std::chrono::milliseconds kFlashPeriod{25};
 const bool kTimerStartActive{true};
+const units::Degrees kDegreesPerMilliSecond{60.0 / 1000.0};
 
 HeadBumpParticle::HeadBumpParticle(Graphics& graphics,
         Vector<units::Game> center_pos) :
@@ -18,18 +19,22 @@ HeadBumpParticle::HeadBumpParticle(Graphics& graphics,
             units::gameToPixel(kSourceHeight)),
     live_timer_(kLifeTime, kTimerStartActive),
     center_pos_(center_pos),
-    particle_a_(0, 0)
+    particle_a_(32, 0)
 {}
 
 void HeadBumpParticle::draw(Graphics& graphics) const
 {
+    sprite_.draw(graphics,center_pos_);
+
     if (live_timer_.current_time() / kFlashPeriod % 2 == 0) {
-        sprite_.draw(graphics,center_pos_);
+        sprite_.draw(graphics, center_pos_ + particle_a_.getPos());
     }
 }
 
 bool HeadBumpParticle::update(const std::chrono::milliseconds elapsed_time)
 {
+    const auto angle_diff = kDegreesPerMilliSecond * elapsed_time.count();
+    particle_a_.setAngle(particle_a_.getAngle() + angle_diff);
+
     return live_timer_.is_active();
-    printf("%ld\n", elapsed_time.count());
 }
--- src/vector.h
+++ src/vector.h
@@ -5,6 +5,10 @@ template <typename T>
 struct Vector {
     T x;
     T y;
+    Vector<T> operator+(const Vector<T>& rhs) const
+    {
+        return Vector{x + rhs.x, y + rhs.y};
+    }
 };
 
 #endif /* VECTOR_H_ */
